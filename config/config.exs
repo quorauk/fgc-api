fa82364a8ec7388fdb1df644dcbc1b0a52473d5a
Max Prettyjohns <heshoots9999@gmail.com>
2019-12-27 16:54:34 +0000

    setup heroku

--- config/config.exs
+++ config/config.exs
@@ -26,7 +26,7 @@ config :logger, :console,
 config :phoenix, :json_library, Jason
 
 config :fgc, Fgc.UserManager.Guardian,
-  issuer: "fgc",
+  issuer: "fgc"
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
