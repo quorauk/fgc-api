0e770d27d20afada679b7ab47055076a3f5ce927
Max Prettyjohns <heshoots9999@gmail.com>
2020-01-11 01:32:25 +0000

    remove unused routes

--- lib/fgc_web/router.ex
+++ lib/fgc_web/router.ex
@@ -18,34 +18,15 @@ defmodule FgcWeb.Router do
   end
 
   pipeline :api do
-    # plug CORSPlug, origin: "http://localhost:3000"
     plug :accepts, ["json"]
   end
-
-  # scope "/", FgcWeb do
-  #   pipe_through [:browser, :auth]
-
-  #   get "/", PageController, :index
-
-  #   get "/login", SessionController, :new
-  #   post "/login", SessionController, :login
-  #   get "/logout", SessionController, :logout
-  # end
-
-  # scope "/", FgcWeb do
-  #   pipe_through [:browser, :auth, :ensure_auth]
-
-  #   get "/protected", PageController, :protected_json
-  # end
 
   scope "/api", FgcWeb do
     pipe_through [:api, :auth]
 
     post "/login", SessionController, :json_login
-    # options "/login", SessionController, :options
 
     delete "/logout", SessionController, :json_logout
-    # options "/logout", SessionController, :options
   end
 
   # Other scopes may use custom stacks.
@@ -53,20 +34,15 @@ defmodule FgcWeb.Router do
     pipe_through [:api, :auth, :ensure_auth]
 
     get "/me", SessionController, :me
-    # options "/me", SessionController, :options
 
     get "/me/webhook_token", SessionController, :webhook_token
-    # options "/me/webhook_token", SessionController, :webhook_token
 
     get "/validate-login", SessionController, :validate_login
 
     get "/me/scoreboards", ScoreboardController, :index
     post "/me/scoreboards", ScoreboardController, :create
-    # options "/me/scoreboards", ScoreboardController, :options
 
     get "/me/scoreboards/:id", ScoreboardController, :get
     delete "/me/scoreboards/:id", ScoreboardController, :delete
-    # options "/me/scoreboards/:id", ScoreboardController, :options
-
   end
 end
